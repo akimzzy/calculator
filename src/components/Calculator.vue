09f69f6c54482aa5ca4a425d0e06f24c54e2acf9
akimzzy <a.akeem@yahoo.com>
2020-10-23 16:39:06 +0100

    increased font size

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -2,10 +2,10 @@
   <div class="bg-dark w-35 h-9.5v flex flex-col rounded-md overflow-hidden">
     <div class="flex-30 p-5 bg-darkk rounded-b-3xl flex flex-col">
 
-      <div class="flex-1 text-white text-right text-lg overflow-x-auto">{{current}}</div>
+      <div class="flex-1 text-white text-right text-3xl overflow-x-auto">{{current}}</div>
 
       <div class="flex justify-between">
-        <small class="text-blue ">{{previous}}</small>
+        <small class="text-blue text-xl">{{previous}}</small>
         <small class="text-blue ">{{sign}}</small>
       </div>
 
